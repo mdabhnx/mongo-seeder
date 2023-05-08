b1c4f7544174b6b3ba0a272defe0693e5300a6a5
Md Abid Hossain <113875870+mdabhnx@users.noreply.github.com>
2023-05-08 17:22:51 +0600

    Update seeder.js

--- src/seeder.js
+++ src/seeder.js
@@ -10,26 +10,46 @@ mongoose.connect("mongodb://127.0.0.1:27017/kerko");
 
 const TARGET_PATH = path.join(__dirname, "data/comapnies/");
 
-fs.readdir(TARGET_PATH, async (err, files) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  console.time("seeding-time");
-  files.map(async (file) => {
-    try {
-      const dataStream = await fs.createReadStream(
-        path.join(TARGET_PATH, file),
-        "utf8"
-      );
-      const parser = JSONStream.parse("*");
-      dataStream.pipe(parser).on("data", (data) => {
-        CompanyModel.collection.insertOne(data);
-      });
-    } catch (error) {
-      log("err", error);
+const startCompaniesSeeding = () => {
+  fs.readdir(TARGET_PATH, async (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
     }
+
+    console.time("seeding-time");
+    files.map(async (file) => {
+      try {
+        const dataStream = await fs.createReadStream(
+          path.join(TARGET_PATH, file),
+          "utf8"
+        );
+        const parser = JSONStream.parse("*");
+        dataStream.pipe(parser).on("data", (data) => {
+          CompanyModel.collection.insertOne(data);
+        });
+      } catch (error) {
+        log("err", error);
+      }
+    });
+    console.timeEnd("seeding-time");
   });
-  console.timeEnd("seeding-time");
-});
+};
+
+const startCompanyDetailsSeeding = () => {
+  const fs = require("fs");
+  const path = require("path");
+
+  function readFilesRecursively(folderPath) {
+    const files = fs.readdirSync(folderPath);
+    files.forEach((file) => {
+      const filePath = path.join(folderPath, file);
+      const stats = fs.statSync(filePath);
+      if (stats.isDirectory()) {
+        readFilesRecursively(filePath);
+      } else {
+        log(stats);
+      }
+    });
+  }
+};
